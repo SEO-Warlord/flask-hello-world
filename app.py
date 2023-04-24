a7d4650fa535ebb5745eb22e21756aa7baaf5785
Angelos <36424692+SEO-Warlord@users.noreply.github.com>
2023-04-24 16:08:42 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
-from autosuggest import get_autosuggest
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/get_autosuggest', methods=['GET'])
-def autosuggest():
-    query = request.args.get('query')
-    suggestions = get_autosuggest(query)
-    return jsonify(suggestions=suggestions)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/get_autosuggest')
+def get_autosuggest():
+    query = request.args.get('query')
+    url = f'https://suggestqueries.google.com/complete/search?output=firefox&q={query}'
+    response = requests.get(url)
+    data = response.json()
+    suggestions = data[1]
+    return {'suggestions': suggestions}
